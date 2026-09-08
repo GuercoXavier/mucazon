48498324daa62a3663698e7422362cdf5d9b8cba
Guerco10 <guercoxavier1@gmail.com>
2026-09-08 15:20:21 +0200

    userController with only an endpoint to test if it runs

--- src/main/java/com/guerco10/mucazon/MucazonApplication.java
+++ src/main/java/com/guerco10/mucazon/MucazonApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MucazonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MucazonApplication.class, args);
-	}
+    static void main(String[] args) {
+
+        SpringApplication.run(MucazonApplication.class, args);
+    }
 
 }
--- src/test/java/com/guerco10/mucazon/MucazonApplicationTests.java
+++ src/test/java/com/guerco10/mucazon/MucazonApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MucazonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
