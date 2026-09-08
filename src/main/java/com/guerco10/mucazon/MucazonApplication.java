2d146bdd4d202c0f24c05b752031d7399667835c
Guerco10 <guercoxavier1@gmail.com>
2026-09-08 15:13:41 +0200

    Initial commit

---
+++ src/main/java/com/guerco10/mucazon/MucazonApplication.java
@@ -0,0 +1,13 @@
+package com.guerco10.mucazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MucazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guerco10/mucazon/MucazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guerco10.mucazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MucazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
